612ba5b54295e7dba58e26a7a74a54db987087b4
Gayeethri1806 <58729665+Gayeethri@users.noreply.github.com>
2024-05-05 21:48:41 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -190,8 +190,7 @@ const workExperiences = {
         "Developed over 20 features for the Enhanced Payment Integrity Compass product.",
          "Upgraded the calculation engine to handle complex payer contracts and achieve 90% accuracy in calculating healthcare claims reimbursement.",
       ]
-    }
-        
+    },   
     {
       role: "Software Developer Intern",
       company: "Optum",
